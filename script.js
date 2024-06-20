607fe59e9c2ae620372c10fedcd62673a4246149
JYOTI-MARKUN <jyotimarkun1997@gmail.com>
2024-06-20 11:05:48 +0530

    Revert "Merge branch 'bugfix'"
    This reverts commit 614f5995e7553ed796a6a8df0674a7a41606ac51, reversing
    changes made to 7a852dc7c8307bff93ae24085cf7408cda9ac30c.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 console.log("Hello");
 console.log("learning git")
 const obj = object.create(null);
-console.log(obj);
+console.log(obj)
 console.log("hellow welcome to the world of js");
-console.log("git and github is completed");
-const obj1 = new object();
+console.log("git and github is completed")
